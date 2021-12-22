262543f4f1831c7436e08ee86bd7069384aada06
gbasiladze <Start120>
2021-12-22 10:30:05 +0300

    fixik

--- src/main/java/com/goganesh/packages/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/goganesh/packages/controller/RestResponseEntityExceptionHandler.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goganesh.packages.exception.ApiException;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
